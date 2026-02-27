7c69ff30bf5dbf9575cf47e793b5c17207ef9738
Willicious <williciousmedia@gmail.com>
2026-02-27 07:50:18 +0000

    Mouse Config Part IX - Removed unnecessary "Off" check
    + Unchecking all other checks has the same effect anyway

--- src/lemmini/gui/MouseConfig.java
+++ src/lemmini/gui/MouseConfig.java
@@ -56,16 +56,11 @@ public class MouseConfig extends JDialog {
             label.setPreferredSize(new Dimension(150, 25));
             row.add(label);
 
-            final JCheckBox checkOff = new JCheckBox("Off");
             final JCheckBox checkLeft = new JCheckBox("Left");
             final JCheckBox checkMiddle = new JCheckBox("Middle");
             final JCheckBox checkRight = new JCheckBox("Right");
             final JCheckBox checkBackward = new JCheckBox("X1");
             final JCheckBox checkForward = new JCheckBox("X2");
-            
-            checkOff.setEnabled(true);
-            final MouseAction thisAction = action;
-            checkOff.addActionListener(e -> checkOffCheckChanged(thisAction, checkOff));
             
             if (!isAllowed(action, MouseEvent.BUTTON1)) checkLeft.setEnabled(false);
             if (!isAllowed(action, MouseEvent.BUTTON2)) checkMiddle.setEnabled(false);
@@ -73,7 +68,6 @@ public class MouseConfig extends JDialog {
             if (!isAllowed(action, 4)) checkBackward.setEnabled(false);
             if (!isAllowed(action, 5)) checkForward.setEnabled(false);
 
-            row.add(checkOff);
             row.add(checkLeft);
             row.add(checkMiddle);
             row.add(checkRight);
@@ -82,7 +76,6 @@ public class MouseConfig extends JDialog {
 
             // Store checkboxes
             Map<Integer, JCheckBox> map = new HashMap<Integer, JCheckBox>();
-            map.put(MouseEvent.NOBUTTON, checkOff);
             map.put(MouseEvent.BUTTON1, checkLeft);
             map.put(MouseEvent.BUTTON2, checkMiddle);
             map.put(MouseEvent.BUTTON3, checkRight);
@@ -175,21 +168,6 @@ public class MouseConfig extends JDialog {
     
     private Map<Integer, JCheckBox> getCheckMap(MouseAction action) {
         return actionCheckBoxes.getOrDefault(action, Collections.emptyMap());
-    }
-    
-    private void updateOffSelection(MouseAction action, boolean offSelected) {
-        Map<Integer, JCheckBox> map = getCheckMap(action);
-        for (Map.Entry<Integer, JCheckBox> e : map.entrySet()) {
-            int button = e.getKey();
-            JCheckBox box = e.getValue();
-            if (button == MouseEvent.NOBUTTON) continue;
-            box.setEnabled(!offSelected && isAllowed(action, button));
-            if (offSelected) box.setSelected(false);
-        }
-    }
-    
-    private void checkOffCheckChanged(MouseAction action, JCheckBox checkOff) {
-        updateOffSelection(action, checkOff.isSelected());
     }
 
 	private String describeAction(MouseAction action) {
