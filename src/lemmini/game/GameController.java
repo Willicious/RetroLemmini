Willicious/RetroLemmini

commit d701412f3baf6d9aef5b33844a1355f56198fea6

Mods can now be defined per-level (preferred) as well as per-pack

src/lemmini/game/GameController.java
@@ -812,8 +812,12 @@ public class GameController {
         curRating = rating;
         curLevelNumber = lNum;
 
+        Resource lvlRes = levelPacks.get(curLevelPack).getInfo(curRating, curLevelNumber).getLevelResource();
+        // loading the level will swap appropriate lemmings pixels to the correct colors
+        level = new Level(lvlRes, level);
+        
         List<String> oldMods = modPaths;
-        modPaths = levelPacks.get(curLevelPack).getModPaths();
+        modPaths = level.getMods(); // per-level mods override per-pack mods
         if (!modPaths.equals(oldMods)) {
             sound.load();
             MiscGfx.init(ToolBox.scale(width, 1.0 / 16.0));
@@ -825,10 +829,6 @@ public class GameController {
             LemmCursor.init();
             Lemming.loadLemmings();
         }
-
-        Resource lvlRes = levelPacks.get(curLevelPack).getInfo(curRating, curLevelNumber).getLevelResource();
-        // loading the level will swap appropriate lemmings pixels to the correct colors
-        level = new Level(lvlRes, level);
 
         initLevel(true);
 

src/lemmini/game/Level.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Locale;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.BooleanUtils;
@@ -156,6 +157,7 @@ public class Level {
     private final List<Steel> steel;
     private final Background[] backgrounds;
     private final GraphicsBuffer[][] bgBuffers;
+    private final List<String> levelMods; // per-level mods
     /** level name - originally 32 bytes ASCII filled with whitespace */
     private final String lvlName;
     private final String author;
@@ -195,6 +197,13 @@ public class Level {
             levelProps.add(p);
             mainLevel = p.get("mainLevel", StringUtils.EMPTY);
         }
+        
+        // read per-level mods
+        String[] modsStr = Props.getArray(levelProps, "mods", null);
+        if (modsStr != null && modsStr.length > 0)
+            levelMods = Arrays.stream(modsStr).map(mod -> "mods/" + mod).collect(Collectors.toList());
+        else
+        	levelMods = GameController.getCurLevelPack().getModPaths(); // fallback to level pack mods
 
         // read name and author
         lvlName = Props.get(levelProps, "name", StringUtils.EMPTY);
@@ -1722,4 +1731,8 @@ public class Level {
             this.scale = scale;
         }
     }
+
+	public List<String> getMods() {
+		return levelMods;
+	}
 }

src/lemmini/game/LevelPack.java
@@ -77,7 +77,7 @@ public class LevelPack {
     private int codeOffset;
 
     private final List<String> debriefings = new ArrayList<>(9);
-    private final List<String> mods;
+    private final List<String> packMods; // per-pack mods
 
     /**
      * Constructor for dummy level pack. Needed for loading single levels.
@@ -89,7 +89,7 @@ public class LevelPack {
         codeSeed = StringUtils.EMPTY;
         maxFallDistance = 126;
         codeOffset = 0;
-        mods = Collections.emptyList();
+        packMods = Collections.emptyList();
 
         ratings.add("Single Levels");
 
@@ -124,7 +124,7 @@ public class LevelPack {
         allLevelsUnlocked = props.getBoolean("allLevelsUnlocked", false);
         // read mods
         String[] modsStr = props.getArray("mods", ArrayUtils.EMPTY_STRING_ARRAY);
-        mods = Arrays.stream(modsStr).map(modStr -> "mods/" + modStr).collect(Collectors.toList());
+        packMods = Arrays.stream(modsStr).map(modStr -> "mods/" + modStr).collect(Collectors.toList());
         // read code seed
         codeSeed = props.get("codeSeed", StringUtils.EMPTY).trim().toUpperCase(Locale.ROOT);
         // read code level offset
@@ -321,6 +321,6 @@ public class LevelPack {
     }
 
     public List<String> getModPaths() {
-        return Collections.unmodifiableList(mods);
+        return Collections.unmodifiableList(packMods);
     }
 }

commit 16b3e0f132273d40913d355eba10d83de7c1c0a3

Bugfix - Apply sprite recoloring after loading mods
+ Sprite recoloring needed to be decoupled from the Level constructor so that it can be applied to mods (this broke in commit d701412 when moving level loading to before mods loading)

src/lemmini/game/GameController.java
@@ -853,6 +853,8 @@ public class GameController {
             LemmCursor.init();
             Lemming.loadLemmings();
         }
+        
+        Lemming.replaceColors(level.getDebrisColor(), level.getDebrisColor2());
 
         initLevel(true);
 

src/lemmini/game/Level.java
@@ -450,8 +450,6 @@ public class Level {
             stencil = level2.stencil;
             level2.stencil = null;
         }
-
-        Lemming.replaceColors(getDebrisColor(), getDebrisColor2());
     }
 
     void createLevelImage() {