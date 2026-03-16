581dabdb2f192d0be24fc8b54620f3d6e9db0ec7
Willicious <williciousmedia@gmail.com>
2026-03-16 14:15:18 +0000

    === Revision 2.9 ===

--- src/lemmini/game/Core.java
+++ src/lemmini/game/Core.java
@@ -71,7 +71,7 @@ import lemmini.tools.ToolBox;
  */
 public class Core {
 
-    public static final String REVISION = "2.8";
+    public static final String REVISION = "2.9";
     public static final String COMMIT_ID = CommitID.ID;
     public static final String REV_DATE = "March 2026";
 
