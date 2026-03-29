1c6ff2463380dd49e57e06f0438f0d54fc56bc05
Willicious <williciousmedia@gmail.com>
2026-03-29 04:32:07 +0100

    === Version 2.9.5 ===

--- src/lemmini/game/Core.java
+++ src/lemmini/game/Core.java
@@ -62,7 +62,7 @@ import lemmini.tools.ToolBox;
  */
 public class Core {
 
-    public static final String REVISION = "2.9.4";
+    public static final String REVISION = "2.9.5";
     public static final String COMMIT_ID = CommitID.ID;
     public static final String REV_DATE = "April 2026";
 
